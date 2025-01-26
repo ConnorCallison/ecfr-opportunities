a049e8eee4542bf22f514fdb6d6eff2058507565
Connor Callison <connorcallison@gmail.com>
2025-01-26 14:23:19 -0800

    Remix route to test fetching some data via the database package

--- apps/ecfrop-web/app/root.tsx
+++ apps/ecfrop-web/app/root.tsx
@@ -1,30 +1,20 @@
+import type { LinksFunction, MetaFunction } from '@remix-run/node';
 import {
   Links,
+  LiveReload,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
 } from '@remix-run/react';
-import type { MetaFunction, LinksFunction } from '@remix-run/node';
+import styles from './tailwind.css';
 
 export const meta: MetaFunction = () => [
-  {
-    title: 'New Remix App',
-  },
+  { title: 'eCFR Opportunities' },
+  { name: 'description', content: 'Explore and analyze eCFR regulations' },
 ];
 
-export const links: LinksFunction = () => [
-  { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
-  {
-    rel: 'preconnect',
-    href: 'https://fonts.gstatic.com',
-    crossOrigin: 'anonymous',
-  },
-  {
-    rel: 'stylesheet',
-    href: 'https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap',
-  },
-];
+export const links: LinksFunction = () => [{ rel: 'stylesheet', href: styles }];
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -45,5 +35,20 @@ export function Layout({ children }: { children: React.ReactNode }) {
 }
 
 export default function App() {
-  return <Outlet />;
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
 }
